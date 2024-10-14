a4b40e29a61035b21f54f42146ba7f5493a75b55
tonibuenter <toni@1234.ch>
2024-10-14 08:46:55 +0200

    Add Welcome Title (.env)

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -3,7 +3,9 @@ import { Button, Stack, Tooltip } from '@mui/material';
 import { ConnectWithLocalstore } from './ConnectWithLocalstore';
 import { ConnectWithMetamaskButton } from './ConnectWithMetamaskButton';
 import { LoginFragment } from './LoginFragment';
+import { DivBox } from '../common/DivBox';
 
+const title = process.env.REACT_APP_TITLE;
 export const Login: React.FC = () => {
   const [openConnectDirect, setOpenConnectDirect] = useState(false);
 
@@ -11,6 +13,7 @@ export const Login: React.FC = () => {
     <LoginFragment
       content={
         <Stack key={'login-buttons'}>
+          {!!title && <DivBox sx={{ margin: '1em' }}>{title}</DivBox>}
           <ConnectWithMetamaskButton key={'connect-with-metamask'} />
           <Tooltip key={'connect-with-localstore'} title={'Use Wallet from Browsers Local Storage.'}>
             <Button onClick={() => setOpenConnectDirect(true)}>Connect With LocalStorage</Button>
--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import { DApp } from './DApp';
-
+import './i18n';
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
