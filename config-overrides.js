50abafcee7bd47bca3cac3ab15bca73907f50fb2
tonibuenter <toni@1234.ch>
2024-11-20 14:31:56 +0100

    Working on IrysAccess

--- config-overrides.js
+++ config-overrides.js
@@ -1,29 +1,30 @@
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
 const webpack = require('webpack');
 
 module.exports = function override(config) {
+  config.plugins = (config.plugins || []).concat([
+    new NodePolyfillPlugin(),
+    new webpack.ProvidePlugin({
+      process: 'process/browser.js'
+    })
+  ]);
+
   const fallback = config.resolve.fallback || {};
   Object.assign(fallback, {
     crypto: require.resolve('crypto-browserify'),
     stream: require.resolve('stream-browserify'),
-    zlib: require.resolve('browserify-zlib'),
     assert: require.resolve('assert'),
     http: require.resolve('stream-http'),
     https: require.resolve('https-browserify'),
     os: require.resolve('os-browserify'),
     url: require.resolve('url'),
-    'process/browser': require.resolve('process/browser'),
-    vm: require.resolve('vm-browserify')
+    zlib: require.resolve('browserify-zlib'),
+    buffer: require.resolve('buffer'),
+    path: require.resolve('path-browserify')
   });
+
   config.resolve.fallback = fallback;
-  config.plugins = (config.plugins || []).concat([
-    new webpack.ProvidePlugin({
-      process: 'process/browser',
-      Buffer: ['buffer', 'Buffer']
-    })
-  ]);
-  // config.module.rules.push({
-  //   test: /\.md/i,
-  //   use: 'raw-loader'
-  // });
+  config.resolve.extensions = ['.js', '.jsx', '.json', '.mjs', '.wasm', '.css'];
+
   return config;
 };
